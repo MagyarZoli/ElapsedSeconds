b9d845bd1e557cd1f1c67f8671efc11fa50ba29e
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-03-14 13:19:47 +0100

    1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+## independent project
+
+## Features
+MZElapsedSeconds
+
+This program returns the seconds elapsed from the current time when the program is started.
+the initial value of the program can be modified with the `resultInAdd()` method.
+With its `runAge()` abstract method, which is connected to the `TimerTask run()` method, it returns continuous accurate results.
---
+++ src/MZElapsedSeconds.java
@@ -0,0 +1,61 @@
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This program returns the seconds elapsed from the current time when the program is started.
+ * the initial value of the program can be modified with the {@code resultInAdd()} method.
+ * With its {@code runAge()} abstract method, which is connected to the {@code TimerTask run()} method, it returns continuous accurate results.
+ * @see java.text.DecimalFormat
+ * @see java.text.ParseException
+ * @see java.text.SimpleDateFormat
+ * @see java.util.Calendar
+ * @see java.util.Date
+ * @see java.util.TimerTask
+ * @see java.util.concurrent.TimeUnit
+ * @since 1.0
+ * @author <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
+ */
+public abstract class MZElapsedSeconds
+extends TimerTask{
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    private static DecimalFormat decimalFormt = new DecimalFormat("###");
+    private static Date date = new Date();
+    private String resultin = simpleDateFormat.format(date);
+    private String resultout;
+
+    private long dateToSeconds(String str, SimpleDateFormat format)
+    throws ParseException{
+        date = format.parse(str);
+        return TimeUnit.MILLISECONDS.toSeconds(date.getTime());
+    }
+
+    @Override
+    public void run(){
+        try{
+            resultout = (decimalFormt.format(dateToSeconds(simpleDateFormat.format(Calendar.getInstance().getTime()), simpleDateFormat)-dateToSeconds(resultin, simpleDateFormat)));
+            runAge();
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    public abstract void runAge();
+    
+    public String resultOut(){
+        return resultout;
+    }
+
+    public String resultIn(){
+        return resultin;
+    }
+
+    public void resultInAdd(String resultin){
+        this.resultin=resultin;
+    }
+}
